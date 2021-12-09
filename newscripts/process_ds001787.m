97388c39685991978f2c97aa60cb02ecc09688ca
Arnaud Delorme <arnodelorme@gmail.com>
2021-12-08 23:05:21 -1000

    fix multiple subject processing

--- newscripts/process_ds001787.m
+++ newscripts/process_ds001787.m
@@ -32,6 +32,7 @@ EEG = pop_select( EEG, 'nochannel',{'EXG1','EXG2','EXG3','EXG4','EXG5','EXG6','E
 for iEEG = 1:length(EEG)
     EEG(iEEG).chanlocs = struct('labels', { 'Fp1' 'AF7' 'AF3' 'F1' 'F3' 'F5' 'F7' 'FT7' 'FC5' 'FC3' 'FC1' 'C1' 'C3' 'C5' 'T7' 'TP7' 'CP5' 'CP3' 'CP1' 'P1' 'P3' 'P5' 'P7' 'P9' 'PO7' 'PO3' 'O1' 'Iz' 'Oz' 'POz' 'Pz' 'CPz' 'Fpz' 'Fp2' 'AF8' 'AF4' 'AFz' 'Fz' 'F2' 'F4' 'F6' 'F8' 'FT8' 'FC6' 'FC4' 'FC2' 'FCz' 'Cz' 'C2' 'C4' 'C6' 'T8' 'TP8' 'CP6' 'CP4' 'CP2' 'P2' 'P4' 'P6' 'P8' 'P10' 'PO8' 'PO4' 'O2' });
 end
+EEG = eeg_checkset(EEG);
 eeglabp = fileparts(which('eeglab.m'));
 EEG = pop_chanedit(EEG, 'lookup',fullfile(eeglabp, 'plugins','dipfit','standard_BEM','elec','standard_1005.elc'));
 chanlocs = EEG(1).chanlocs;
@@ -57,6 +58,7 @@ ALLEEG = EEG;
 % resample extract epochs
 for iEEG = 1:length(ALLEEG)
     EEG = ALLEEG(iEEG);
+    EEG = eeg_checkset(EEG, 'loaddata');
     EEG = pop_resample(EEG, 100);
     EEG = eeg_regepochs(EEG, 2, [0 2]);
 
