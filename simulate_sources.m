147d0783b57dca2611dc9aef4bf594f56573ab3e
Arnaud Delorme <arnodelorme@gmail.com>
2022-11-11 00:41:02 -0800

    fix simulation

--- simulate_sources.m
+++ simulate_sources.m
@@ -1,30 +1,32 @@
 % Requires EEGLAB, ROIconnect, and SIFT installed
+% Simulate 2 sources in the temporal cortex
+
 clear
 eeglab
 
 %% create empty dataset and create leadfield matrix
 eeglabp = fileparts(which('eeglab'));
-sourceModelPath = fullfile(eeglabp, 'functions/supportfiles/head_modelColin27_5003_Standard-10-5-Cap339.mat');
+sourceModelPath = fullfile(eeglabp, 'functions','supportfiles','head_modelColin27_5003_Standard-10-5-Cap339.mat');
 tmpSource   = load('-mat', sourceModelPath);
 chans =  { 'AF7' 'FP1' 'Afz' 'FP2' 'AF8' 'F7' 'F3' 'F1' 'Fz' 'F2' 'F4' 'F8' 'FT7' 'FC5' 'FC3' 'FC1' 'FCz' 'FC2' 'FC4' 'FC6' 'FT8' 'T7' 'C5' 'C3' 'C1' 'Cz' 'C2' 'C4' 'C6' 'T8' 'TP7' 'CP5' 'CP3' 'CP1' 'CPz' 'CP2' 'CP4' 'CP6' 'TP8' 'P7' 'P5' 'P3' 'P1' 'Pz' 'P2' 'P4' 'P6' 'P8' 'P9' 'PO7' 'PO5' 'PO3' 'Poz' 'PO4' 'PO6' 'PO8' 'P10' 'PO9' 'PO9h' 'O1' 'Oz' 'O2' 'PO10h' 'PO10' };
 scalpData   = zeros(length(chans), 200, 30);
 EEG = pop_importdata('dataformat','array','nbchan',0,'data',scalpData,'srate',100,'pnts',0,'xmin',0);
 EEG.chanlocs = struct('labels', chans);
-EEG=pop_chanedit(EEG, 'lookup',fullfile(eeglabp, 'plugins/dipfit/standard_BEM/elec/standard_1005.elc'));
-EEG = pop_dipfit_settings( EEG, 'hdmfile',fullfile(eeglabp, 'plugins/dipfit/standard_BEM/standard_vol.mat'),'coordformat','MNI',...
-    'mrifile',fullfile(eeglabp, 'plugins/dipfit/standard_BEM/standard_mri.mat'),...
-    'chanfile',fullfile(eeglabp, 'plugins/dipfit/standard_BEM/elec/standard_1005.elc'),'coord_transform',[0 0 0 0 0 -1.5708 1 1 1] ,'chansel',[1:EEG.nbchan] );
+dipfitdefs;
+EEG=pop_chanedit(EEG, 'lookup',template_models(2).chanfile);
+EEG = pop_dipfit_settings( EEG, 'hdmfile',template_models(2).hdmfile,'coordformat','MNI',...
+    'mrifile',template_models(2).mrifile,'chanfile',template_models(2).chanfile,'coord_transform',[0 0 0 0 0 -1.5708 1 1 1] ,'chansel',[1:EEG.nbchan] );
 EEG = pop_leadfield(EEG, 'sourcemodel',fullfile(eeglabp, 'functions/supportfiles/head_modelColin27_5003_Standard-10-5-Cap339.mat'),'sourcemodel2mni',[0 -24 -45 0 0 -1.5708 1000 1000 1000] ,'downsample',1);
 leadfield = [ EEG.dipfit.sourcemodel.leadfield{:} ];
 
-%% Simulate VAR model 2 sources coupled
+%% Simulate VAR model 2 sources coupled using SIFT
 %simulations = hlp_getSimExamples;simulations{5}{2} = feval(simulations{5}{2});
 simulations{1} = 'Bivariate Coupled Oscillator';
 simulations{2} = {'x1(t) = 0.6*x1(t-1) +  0.65*x2(t-2)+ e1(t)' 'x2(t) = 0.5*x2(t-1) + -0.3*x2(t-2) + e2(t)'};
 sourceRois = {'temporalpole L' 'temporalpole R' };
 [EEGsim] = sim_varmodel('sim', simulations, 'plotData', false, 'simParams', { 'TrialLength', 2, 'NumTrials',30 });
 
-% project sources onto scalp
+%% project sources onto scalp
 if length(sourceRois) ~= size(EEGsim.data,1), error('Wrong number of sources'); end
 nvert = size(tmpSource.cortex.vertices,1);
 sigma = 10/1000; % same unit as vertices (source spread)
