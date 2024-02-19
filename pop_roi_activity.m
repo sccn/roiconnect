6202c841b8befb08e64ac807661e08debd999979
Hiyeri <48970646+Hiyeri@users.noreply.github.com>
2024-02-19 14:29:13 +0100

    extend documentation

--- pop_roi_activity.m
+++ pop_roi_activity.m
@@ -31,8 +31,8 @@
 %                       specified, the signal is zero padded accordingly.
 %                       Default is 0 (means no padding).
 %  'chansel'          - [cell array of string] channel selection. Default is all.
-%  'lowmemory'        - ['on'|'off'] options to run the code with low memory, though, it might take significantly longer to complete.
-%
+%  'lowmemory'        - ['on'|'off'] Option to run the code with low memory, though, it might take significantly longer to complete. When turned on, the estimation of voxel-wise spectral power 
+%                       will require less memory.
 %
 % Other optional inputs:
 %  All ROI_ACTIVITY parameters are accepted as input and passed on.
